e745c7f90523f4af71a6740c8805d2b1ab44ad83
Carlos V.M Lino <wenikius@gmail.com>
2026-02-16 11:43:36 -0300

    V.4: sistema de puxar cartas aplicável a ambos jogadores, de acordo com controlador de estado.

--- scripts/draw_card.js
+++ scripts/draw_card.js
@@ -1,47 +1,86 @@
+
+// estes seletores são apenas para testes. 
 const deck = document.querySelector('.deck-stack');
+const deck2 = document.querySelector('.deck-stack2');
 const hand = document.querySelector('.hand1');
+const hand2 = document.querySelector('.hand2');
 const flyingCard = document.querySelector('.flying');
+const flying_c = document.querySelector('.flying');
 
 // Estes samples são só para testar a função drawCard dentro da lógica de separação de responsabilidades.
 let state = {
     sample_deck: ['card1', 'card2', 'card3', 'card4', 'card5', 'card6'],
+    sample_deck2: ['card1', 'card2', 'card3', 'card4', 'card5', 'card6'],
     sample_hand: [],
-    draw_amount: 5
+    sample_hand2: [],
+    draw_amount: 1,
+    player: 2
 };
 
-// handler que controla evento de click deste elemento deck:
+// evento de click e handler que controla este evento:
 deck.addEventListener('click', () => {
     state = drawCard(state);
     updateHandUI(state);
     console.log(state);
 });
 
+// sem evento de click:
+function drawPhase(state_obj){
+    if (state_obj.player === 1){
+        state = drawCard(state);
+        updateHandUI(state);
+        console.log(state);
+    } 
+    else if (state_obj.player === 2){
+        // TODO
+    }
+}
+drawPhase(state);
+
 // conceito de imutabilidade aplicado, ou seja, não se modifica o objeto diretamente dentro da função,
 // mas retorna-se um novo objeto para que seja reatribuido ao objeto original.
 function drawCard(state_obj){
+    let stats = {};
 
-    // se state_obj e sua propriedade sample_deck existirem (?) mas não (!) possuírem tamanho, ou seja, length = 0, então retorna
-    // o mesmo objeto (pra evitar retornar undefined, que é o padrão de return);
-    if (!state_obj?.sample_deck?.length) return state_obj;
+    if (state_obj.player === 1){
+        // se state_obj e sua propriedade sample_deck existirem (?) mas não (!) possuírem tamanho, ou seja, length = 0, então retorna
+        // o mesmo objeto (pra evitar retornar undefined, que é o padrão de return);
+        if (!state_obj?.sample_deck?.length) return state_obj;
 
-    let stats = {
-        ...state_obj,
-        sample_deck: [...state_obj.sample_deck],
-        sample_hand: [...state_obj.sample_hand]
-    }; 
+        stats = {
+            ...state_obj,
+            sample_deck: [...state_obj.sample_deck],
+            sample_hand: [...state_obj.sample_hand],
+        }; 
 
-    // stats.sample_deck.length > 0 aqui é necessário para que não aconteça o caso de o deck esvaziar durante o loop e continuar o loop com deck vazio,
-    // por exemplo: suponha que o deck tenha 1 carta, mas o número de puxadas permitidas é 2, se não houver verificação, seria puxada
-    // a carta restante e mais uma 'undefined', de modo que a mão/array ficaria com objetos 'undefined'.
-    for (let i = 0; i < stats.draw_amount && stats.sample_deck.length > 0; i++){
-        stats.sample_hand.push(stats.sample_deck.pop());
+        // stats.sample_deck.length > 0 aqui é necessário para que não aconteça o caso de o deck esvaziar durante o loop e continuar o loop com deck vazio,
+        // por exemplo: suponha que o deck tenha 1 carta, mas o número de puxadas permitidas é 2, se não houver verificação, seria puxada
+        // a carta restante e mais uma 'undefined', de modo que a mão/array ficaria com objetos 'undefined'.
+        for (let i = 0; i < stats.draw_amount && stats.sample_deck.length > 0; i++){
+            stats.sample_hand.push(stats.sample_deck.pop());
+        }
     }
+    else if (state_obj.player === 2){
+        if (!state_obj?.sample_deck2?.length) return state_obj;
+
+        stats = {
+            ...state_obj,
+            sample_deck2: [...state_obj.sample_deck2],
+            sample_hand2: [...state_obj.sample_hand2],
+        }; 
+
+        for (let i = 0; i < stats.draw_amount && stats.sample_deck2.length > 0; i++){
+            stats.sample_hand2.push(stats.sample_deck2.pop());
+        }
+    }
+
     return stats;
 }
 
 // update de UI com base nos status:
 // TODO: deve ser adicionada uma lógica que faz com que a drawAnimation só seja chamada quando for permitida
 // pois no estado atual, a animação ocorre toda vez que se clique no deck.
+// basicamente, um estado deverá controlar esta animação.
 function updateHandUI(state_obj){
     const stats = {...state_obj}; 
     drawAnimation(stats);
@@ -51,26 +90,53 @@ function updateHandUI(state_obj){
 // TODO: esta animação foi "hard-coded" no momento. 
 // o ideal é que ela crie o elemento flyingCard no container do deck certo,
 // de modo que valores como coordenadas para animação possam ser passados, já que esta função
-// deverá ser usada para ambos os decks. Do jeito que tá agora é preciso fazer um monte de trabalho manual.
+// deverá ser usada para ambos os decks. Do jeito que tá agora é preciso fazer um monte de trabalho repetido.
 function drawAnimation(state_obj){
-    flyingCard.classList.remove("hidden");
 
-    // força reflow
-    flyingCard.offsetHeight;
+    if (state_obj.player === 1){
+        flyingCard.classList.remove("hidden");
 
-    // inicia animação
-    flyingCard.classList.add("move");
+        // força reflow
+        flyingCard.offsetHeight;
 
-    flyingCard.addEventListener("transitionend", () => {
-      // cria carta definitiva na mão
-      const card = document.createElement("div");
-      card.className = "hoverable card";
-      hand.appendChild(card);
+        // inicia animação
+        flyingCard.classList.add("move");
 
-      // esconde e reseta sem animação
-      flyingCard.classList.remove("move");
-      flyingCard.classList.add("hidden");
-    }, { once: true });
+        flyingCard.addEventListener("transitionend", () => {
+        // cria carta definitiva na mão
+        const card = document.createElement("div");
+        card.className = "hoverable card";
+        hand.appendChild(card);
+
+        // esconde e reseta a animação.
+        flyingCard.classList.remove("move");
+        flyingCard.classList.add("hidden");
+        }, {once: true});
+
+        // {once: true} evita que o evento fique acumulando no call stack, 
+        // ou seja, ele é executado apenas uma vez e então removido do call stack.
+        // isto evita bug de eventos duplicados a cada nova chamada.
+    }
+    else if (state_obj.player === 2){ 
+        flying_c.classList.remove("hidden");
+
+        // força reflow
+        flying_c.offsetHeight;
+
+        // inicia animação
+        flying_c.classList.add("move2");
+
+        flying_c.addEventListener("transitionend", () => {
+        // cria carta definitiva na mão
+        const card = document.createElement("div");
+        card.className = "hoverable card";
+        hand2.appendChild(card);
+
+        // esconde e reseta a animação.
+        flying_c.classList.remove("move2");
+        flying_c.classList.add("hidden");
+        }, {once: true});
+    }
 }
 
 
--- scripts/select_field.js
+++ scripts/select_field.js
@@ -10,11 +10,32 @@ let SELECTION_STATE = {
     }
 }
 
-// tem que criar um event handler pra poder capturar as novas cartas com elemento .hoverable.
-for (let card of cards){
-    card.addEventListener("mouseover", () => onMouseOver(card, SELECTION_STATE));
-    card.addEventListener("mouseout", () => onMouseOut(card));
-    card.addEventListener("click", () => onMouseClick(card, cards, SELECTION_STATE));
+// eventos e handlers:
+// TODO: é possível refatorar para eliminar estas repetições de chamadas, embora não seja necessário aqui.
+document.body.addEventListener('mouseover', node => selectField(node, 'mouseover'));
+document.body.addEventListener('mouseout', node => selectField(node, 'mouseout'));
+document.body.addEventListener('click', node => selectField(node, 'click'));
+
+// TODO: o uso de closest('.hoverable') é vulnerável, pois uma vez que o elemento não possua tal classe, a UI quebra.
+// melhor é usar algum tipo de identificador para acessar o estado do campo, e então definir se o campo é hoverable ou 
+// a partir de lógica de estado, não de regras de estilo. 
+// Também a seleção de itens deve ocorrer por dataset.id, não por classe.
+function selectField(node, event){
+    if(node.target.closest('.hoverable')){
+        switch (event){
+            case 'mouseover':
+                onMouseOver(node.target);
+                break;
+            case 'mouseout':
+                onMouseOut(node.target);
+                break;
+            case 'click':
+                //TODO
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 function onMouseOver(node) {
@@ -25,26 +46,28 @@ function onMouseOut(node) {
     node.classList.remove('onHover');
 }
 
+// por hora, neste projeto, ainda não há um "type" implícito em cada card. Deverá haver somente para alguns cards
+// que estejam já em campo.
 function onMouseClick(node, nodeList, state) {
     // se o card a ser selecionado é do campo do player:
-        if (node.dataset.type === SELECTION_STATE.CARD_OWNER.player){
+        if (node.dataset.type === state.CARD_OWNER.player){
             // UI
             nodeList.forEach(node => {node.classList.remove('selected'),node.classList.remove('battleSelected')});
             node.classList.remove('onHover');
             node.classList.add('selected');
             // Logic
-            SELECTION_STATE.selectedEnemyCardId = undefined;
-            SELECTION_STATE.selectedPlayerCardId = node.dataset.id;
-            console.log(SELECTION_STATE);
+            state.selectedEnemyCardId = undefined;
+            state.selectedPlayerCardId = node.dataset.id;
+            console.log(state);
         } 
 
         // se existe id registrado para um card do campo do player e o card a ser selecionado é do campo inimigo:
-        if (node.dataset.type === SELECTION_STATE.CARD_OWNER.enemy && SELECTION_STATE.selectedPlayerCardId){
+        if (node.dataset.type === state.CARD_OWNER.enemy && state.selectedPlayerCardId){
             nodeList.forEach(node => node.classList.remove('battleSelected'));
             node.classList.remove('onHover');
             node.classList.add('battleSelected');
 
-            SELECTION_STATE.selectedEnemyCardId = node.dataset.id;
-            console.log(SELECTION_STATE);
+            state.selectedEnemyCardId = node.dataset.id;
+            console.log(state);
         }
 }
