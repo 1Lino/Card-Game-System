d3075a10575b58930aeb3ba23a6a10b939e9c3b6
Lino <wenikius@gmail.com>
2026-03-21 10:22:50 -0300

    V.22.9: algumas refatorações iniciais no código.

---
+++ scripts/Main.js
@@ -0,0 +1,9 @@
+import '/scripts/engine/game_engine.js';
+import '/scripts/engine/game_phases.js';
+import '/scripts/engine/game_state.js';
+import '/scripts/actions/handle_events.js';
+import '/scripts/actions/draw_card.js';
+import '/scripts/actions/use_card.js';
+import '/scripts/UI/render_hand.js';
+
+console.log("Everything Imported!");
--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-import '/scripts/engine/game_engine.js';
-import '/scripts/engine/game_phases.js';
-import '/scripts/engine/game_state.js';
-import '/scripts/actions/handle_events.js';
-import '/scripts/actions/draw_card.js';
-import '/scripts/actions/use_card.js';
-import '/scripts/UI/render_hand.js';
-
-console.log("Everything Imported!");
